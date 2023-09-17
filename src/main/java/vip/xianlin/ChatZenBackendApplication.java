aa57963c147167e76935fb105ab4e7b4d14036b5
XianLin <15653674386@163.com>
2023-09-17 19:24:01 +0800

    提交SpringBoot3初始代码

---
+++ src/main/java/vip/xianlin/ChatZenBackendApplication.java
@@ -0,0 +1,13 @@
+package vip.xianlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatZenBackendApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ChatZenBackendApplication.class, args);
+    }
+    
+}
