fcaa455258fe92017ba0fb58af730ff6b600cab0
XianLin <15653674386@163.com>
2023-11-09 09:41:51 +0800

    修改无效参数

--- src/main/java/vip/xianlin/entity/vo/request/UserAuthVo.java
+++ src/main/java/vip/xianlin/entity/vo/request/UserAuthVo.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import vip.xianlin.utils.DataEnum;
 
 @Data
 @Tag(name = "用户认证信息", description = "用于用户登录")
@@ -19,7 +18,4 @@ public class UserAuthVo {
     
     @Schema(description = "是否记住登录状态")
     boolean rememberMe;
-    
-    @Schema(description = "用户状态")
-    DataEnum.UserOnlineStatus status;
 }
