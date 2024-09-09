d7a671c85e0a461beeb0d53b6f49cdc8ac7dd88b
XianLin <15653674386@163.com>
2024-09-09 14:39:10 +0800

    初始项目，设置端口为8080

---
+++ src/main/java/top/chatzen/chatzenspring/ChatZenSpringApplication.java
@@ -0,0 +1,13 @@
+package top.chatzen.chatzenspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatZenSpringApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ChatZenSpringApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/top/chatzen/chatzenspring/ChatZenSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package top.chatzen.chatzenspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatZenSpringApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
