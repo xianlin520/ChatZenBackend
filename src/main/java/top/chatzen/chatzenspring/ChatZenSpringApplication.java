53c86226dfa009fcb81cbea68a7f098b56eeb382
XianLin <15653674386@163.com>
2024-09-09 16:01:08 +0800

    完善基础信息

--- src/main/java/top/chatzen/chatzenspring/ChatZenSpringApplication.java
+++ src/main/java/top/chatzen/chatzenspring/ChatZenSpringApplication.java
@@ -1,13 +1,18 @@
 package top.chatzen.chatzenspring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatZenSpringApplication {
     
+    private static final Logger log = LoggerFactory.getLogger(ChatZenSpringApplication.class);
+    
     public static void main(String[] args) {
         SpringApplication.run(ChatZenSpringApplication.class, args);
+        log.info("项目启动成功 === SpringBoot3.3.3 - JDK 21");
     }
     
 }
